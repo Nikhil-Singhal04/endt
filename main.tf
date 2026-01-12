f6e1d548c57af7da6c17584a762064a025b46b68
Nikhil Singhal <nikhilsinghal2019@gmail.com>
2026-01-12 11:27:15 +0530

    files added

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "ssh_sg" {
   }
 }
 
-# EC2 Instance created
+# EC2 Instance 
 resource "aws_instance" "my_ec2" {
   ami                    = "ami-0ecb62995f68bb549"   # Amazon Linux 2 (Mumbai)
   instance_type          = "t2.micro"
